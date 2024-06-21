711220ee6bb96d5c51b20f651c37d2c6ab1aedcc
Alvaro Cuba <76538507+DjAlvaroCuba@users.noreply.github.com>
2024-06-20 23:53:39 -0500

    Delete Documents/CICLO5/APLICACIONES MOVILES MULTIPLATAFORMA/SEMANA 9/push_lab9/lib directory

--- Documents/CICLO5/APLICACIONES MOVILES MULTIPLATAFORMA/SEMANA 9/push_lab9/lib/routes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:lab9_cuba/page1.dart';
-import "package:lab9_cuba/page2.dart";
-
-
-Map<String, WidgetBuilder> routes = {
-  '/': (context) => Page1(),
-  '/page2': (context) => Page2(),
-};
